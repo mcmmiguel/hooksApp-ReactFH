2b3a9ef4e89e1ce92aa69870925571d3c579fc34
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-02 19:56:55 -0600

    useCounter created

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { HooksApp } from './HooksApp'
 import { CounterApp } from './01-useState/CounterApp'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </React.StrictMode>,
 )
