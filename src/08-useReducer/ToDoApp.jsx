321c41b63562bdfbb9d7a56b1465704539fe89f8
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-03 20:24:47 -0600

    Design of TODO app done

--- src/08-useReducer/ToDoApp.jsx
+++ src/08-useReducer/ToDoApp.jsx
@@ -16,22 +16,53 @@ const initialState = [
 
 export const ToDoApp = () => {
 
-    const [state, dispatch] = useReducer(toDoReducer, initialState)
+    const [todos, dispatch] = useReducer(toDoReducer, initialState)
 
 
     return (
         <>
 
-            <h1>ToDo App</h1>
+            <h1>ToDo App: 10, <small>pendientes: 2</small> </h1>
             <hr />
 
-            <ul>
+            <div className="row">
 
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
+                <div className="col-7">
+                    <ul className="list-group">
 
-            </ul>
+                        {
+                            todos.map((todo) => (
+                                <li key={todo.id} className="list-group-item d-flex justify-content-between">
+                                    <span className="align-self-center">Item 1</span>
+                                    <button className="btn btn-danger">Borrar</button>
+                                </li>
+                            ))
+                        }
+
+                    </ul>
+                </div>
+
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="¿Qué hay que hacer?"
+                            className="form-control"
+                        />
+
+                        <button
+                            type="submit"
+                            className="btn btn-outline-primary mt-2"
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+
 
         </>
     )
