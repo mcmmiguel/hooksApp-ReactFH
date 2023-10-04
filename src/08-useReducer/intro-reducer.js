4ac822aec64f95ab32582358f11ce803e432dc41
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-03 19:49:18 -0600

    How a reducer works

--- src/08-useReducer/intro-reducer.js
+++ src/08-useReducer/intro-reducer.js
@@ -6,9 +6,37 @@ const initialState = [{
     done: false,
 }]
 
-const toDoReducer = (state = initialState, action) => {
+const toDoReducer = (state = initialState, action = {}) => {
 
-
+    if (action.type === '[TODO] add todo') {
+        return [...state, action.payload];
+    }
 
     return state;
 }
+
+
+let todos = toDoReducer();
+
+const newTodo = {
+    id: 2,
+    todo: 'Recolectar la gema del espacio',
+    done: false,
+};
+
+const addToDoAction = {
+    type: '[TODO] add todo',
+    payload: newTodo,
+}
+
+todos = toDoReducer(todos, addToDoAction);
+
+console.log('STATE:', todos);
+
+
+// !!!!!! No se debe mutar el state !!!!!!
+// todos.push({
+//     id: 2,
+//     todo: 'Recolectar la gema del espacio',
+//     done: false,
+// });
