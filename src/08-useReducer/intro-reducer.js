b020d7956848c1592ff38d65eb6964ec5e4a5eb4
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-03 19:52:41 -0600

    Description of the reducer

--- src/08-useReducer/intro-reducer.js
+++ src/08-useReducer/intro-reducer.js
@@ -1,5 +1,5 @@
 
-
+// Initial state
 const initialState = [{
     id: 1,
     todo: 'Recolectar la piedra del alma',
@@ -8,27 +8,32 @@ const initialState = [{
 
 const toDoReducer = (state = initialState, action = {}) => {
 
-    if (action.type === '[TODO] add todo') {
+    if (action.type === '[TODO] add todo') { // We control our actions with conditional statements
         return [...state, action.payload];
     }
 
-    return state;
+    return state; // If anything happens we return the same state without modifications
 }
 
 
 let todos = toDoReducer();
 
+
+// We want to add a new object to the state
 const newTodo = {
     id: 2,
     todo: 'Recolectar la gema del espacio',
     done: false,
 };
 
+
+// We define our action with its type and payload. The payload is what we want to add
 const addToDoAction = {
     type: '[TODO] add todo',
     payload: newTodo,
 }
 
+// We add our newTodo through our actions. That's the way to return a new state
 todos = toDoReducer(todos, addToDoAction);
 
 console.log('STATE:', todos);
